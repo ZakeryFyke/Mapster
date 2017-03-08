1048f158e28791ad379c642621d1d874cebcba13
Ryan Berg <bergryan>
2017-03-07 21:03:26 -0600

    commented up some functions.. poorly

--- controllers/static.go
+++ controllers/static.go
@@ -2,14 +2,15 @@ package controllers
 
 import "github.com/RyanMcBerg/Mapster/views"
 
-//NewStatic returns a new static
+//Controller for all of our static pages, which will likely be most of our pages.
+
 func NewStatic() *Static {
 	return &Static{
 		Home: views.NewView("bootstrap", "static/home"),
 	}
 }
 
-// Static woo
+// Static Struct to hold our static views, add to this if we have more static pages.
 type Static struct {
 	Home *views.View
 }
--- views/views.go
+++ views/views.go
@@ -6,7 +6,6 @@ import (
 	"path/filepath"
 )
 
-//LayoutDir a
 var ViewsDir string = "views"
 var ViewsExt string = ".gohtml"
 var LayoutDir string = ViewsDir + "/layouts"
@@ -19,7 +18,7 @@ func layoutFiles() []string {
 	return files
 }
 
-//NewView sa
+//NewView will add the prefixes and suffixes and return a View
 func NewView(layout string, files ...string) *View {
 	addViewsDirPrefix(files)
 	addViewExtSuffix(files)
@@ -35,28 +34,31 @@ func NewView(layout string, files ...string) *View {
 	}
 }
 
-//View 	a
+//View, This is a View
 type View struct {
 	Template *template.Template
 	Layout   string
 }
 
-//Render w
+//Render Fucntion is required by gorillamux packages
 func (v *View) Render(w http.ResponseWriter, data interface{}) error {
 	w.Header().Set("Content-Type", "text/html")
 	return v.Template.ExecuteTemplate(w, v.Layout, data)
 }
 
+//ServeHTTP function requried by gorillamux Package...capitalization is important :/
 func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	v.Render(w, nil)
 }
 
+//Function to add directory prefixes so i dont have to manually from now on
 func addViewsDirPrefix(files []string) {
 	for i, f := range files {
 		files[i] = ViewsDir + "/" + f
 	}
 }
 
+//Function to add .gohtml extensions so I don't have to manually
 func addViewExtSuffix(files []string) {
 	for i, f := range files {
 		files[i] = f + ViewsExt
